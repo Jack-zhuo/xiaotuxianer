1b011b8d6dcafe6997f0bcb864736d25f689d086
zhuoyue <zyjava@qq.com>
2023-08-02 13:56:32 +0800

    一天干别人三天干的活，你一年的工作量就是别人三年的。加油！

--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,4 +1,12 @@
 <template>
-  <div>我是主页</div>
+  <layout-nav></layout-nav>
+  <layout-header></layout-header>
   <router-view></router-view>
+  <layout-footer></layout-footer>
 </template>
+
+<script setup>
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
+</script>
